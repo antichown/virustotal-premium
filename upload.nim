3408deb9cc93e8629b1f6ba2cd9c58c75ca2ee2c
0x94 <antichown@gmail.com>
2021-08-07 23:16:12 +0300

    Update upload.nim

--- upload.nim
+++ upload.nim
@@ -3,7 +3,7 @@ import httpclient
 var client = newHttpClient()
 var data = newMultipartData()
 data["apikey"] = "4e3202fdbe953d628f650229af5b3eb49cd46b2d3bfe5546ae3c5fa48b554e0c"
-data.addFiles({"file": "upload.exe"},)
+data.addFiles({"file": "upload.exe"})
 
 let source=client.postContent("https://www.virustotal.com/vtapi/v2/file/scan", multipart=data)
 echo(source)

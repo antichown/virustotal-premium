e4f20719a4f5f5b491b6ff29063f0d43e3d84d1a
0x94 <antichown@gmail.com>
2021-08-07 23:15:08 +0300

    Add files via upload

---
+++ upload.nim
@@ -0,0 +1,9 @@
+import httpclient
+
+var client = newHttpClient()
+var data = newMultipartData()
+data["apikey"] = "4e3202fdbe953d628f650229af5b3eb49cd46b2d3bfe5546ae3c5fa48b554e0c"
+data.addFiles({"file": "upload.exe"},)
+
+let source=client.postContent("https://www.virustotal.com/vtapi/v2/file/scan", multipart=data)
+echo(source)
